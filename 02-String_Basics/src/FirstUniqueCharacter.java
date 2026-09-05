9295c00c4f46e5e26b255e7cdbf432c8598c9697
sandipmryadav <151117657+sandipmryadav@users.noreply.github.com>
2026-09-05 21:01:18 +0530

    "Added optimized string frequency patterns for Anagram and Unique Character"

---
+++ 02-String_Basics/src/FirstUniqueCharacter.java
@@ -0,0 +1,18 @@
+public class FirstUniqueCharacter {
+    public static int uniqueCharacter (String s) {
+        int [] count = new int[26];
+
+        for(int i=0; i<s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for(int i=0; i<s.length(); i++) {
+            if(count[s.charAt(i) - 'a'] == 1) return i ;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("level " + uniqueCharacter("level"));
+    }
+}
