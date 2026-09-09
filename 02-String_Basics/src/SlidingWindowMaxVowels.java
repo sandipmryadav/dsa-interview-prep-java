671960f332afbddc960a00f335163b54cc2efc65
sandipmryadav <151117657+sandipmryadav@users.noreply.github.com>
2026-09-09 23:19:04 +0530

    "Implemented fixed-size sliding window to find max vowels in O(N) time"

---
+++ 02-String_Basics/src/SlidingWindowMaxVowels.java
@@ -0,0 +1,41 @@
+public class SlidingWindowMaxVowels {
+
+    public static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    public static int maxVowels(String s , int k) {
+        int count = 0;
+        int maxCount = 0;
+
+
+        for(int i = 0; i < k; i++) {
+            if(isVowel(s.charAt(i))) {
+                count++;
+            }
+        }
+        maxCount = count;
+
+
+        for(int i = k; i < s.length(); i++) {
+            char newChar = s.charAt(i);
+            char oldChar = s.charAt(i - k);
+
+            if(isVowel(newChar)) {
+                count++;
+            }
+
+            if(isVowel(oldChar)) {
+                count--;
+            }
+
+            maxCount = Math.max(maxCount, count);
+        }
+
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxVowels("abciiidef", 3)); // Output 3 aana chahiye
+    }
+}
